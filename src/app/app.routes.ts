b9b332d946f6a928c0b3f8acdc28af14229b0757
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2025-11-18 19:13:28 +0530

    Home About Contact Page and Navbar Footer Component Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // For Normal Imports
+
+    // {
+    //     path: '',
+    //     component: Home
+    // },
+    // {
+    //     path: 'about',
+    //     component: About
+    // },
+    // {
+    //     path: 'contact',
+    //     component: Contact
+    // }
+
+    // For Lazy Loading
+    {
+        path: '',
+        loadComponent: () => import('./home/home').then(m => m.Home)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./about/about').then(m => m.About)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./contact/contact').then(m => m.Contact)
+    }
+];
